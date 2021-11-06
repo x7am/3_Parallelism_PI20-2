1e8dc79bc4e95bafcce07f048e752e0a0a832dbb
Цветков Семён <65652418+x7am@users.noreply.github.com>
2021-11-06 21:32:20 +0300

    updated

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
+from multiprocessing import Process, Pool
+from json import load
+import numpy as np
+import csv
 
-
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
+def element(args):
+    index, A, B = args[0], args[1], args[2]
+    i, j = index; res = 0
+    N = A.shape[1] or B.shape[0]
     for k in range(N):
-        res += A[i][k] * B[k][j]
+        res += A[i, k] * B[k, j]
     return res
 
+def check_matrix(matrix1, matrix2):
+    print('matrix can exist') if matrix1.shape[1]==matrix2.shape[0] else print('matrix cant exist')
+    
+def generate_map(mt1, mt2):
+    args = []
+    for i in range(mt1.shape[0]):
+        for j in range(mt2.shape[1]):
+            args.append(((i, j), mt1, mt2))
+    print(args)
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
 
-print(element((1, 0), matrix1, matrix2))
+if __name__ == "__main__":
+    with open('matrix1.json','r') as f:
+        matrix1 = np.matrix(load(f))
+
+    with open('matrix2.json','r') as f:
+        matrix2 = np.matrix(load(f))
+
+    p = Pool(matrix1.shape[0] * matrix2.shape[1])
+
+    if check_matrix(matrix1, matrix2):
+        args = generate_map(matrix1, matrix2)
+        result = np.array(p.map(element, args))
+        matrix = result.reshape(matrix1.shape[0], matrix2.shape[1])
+      
+        filename = input('filename')
+        file = open(filename, 'w')
+        for line in matrix:
+            for elem in line:
+                file.write(f'{elem}\t')
+            file.write('\n')
+         file.close()
+       
+    else:
+        file = open('result.txt', 'w')
+        file.close()
